b7c05fc72e8fb4a2d9fa268697c2245b078829e4
Kiruthigadurairaj <146357922+Kiruthigadurairaj@users.noreply.github.com>
2024-12-22 20:45:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,15 +3,22 @@ function mincost(arr)
 //write your code here
 // return the min cost
   if (arr.length<=1)return 0;
+	
 	const MinHeap = require("heap").MinHeap;
 	const heap=new MinHeap((a,b)=>a-b);
+	
 	arr.forEach(length => heap.push(length));
     let totalCost = 0;
-	while (heap.size() > 1) {
+	
+	while (heap.size() > 1) 
+	{
+		
 	const first = heap.pop();
         const second = heap.pop();
+		
         const combinedCost = first + second;
         totalCost += combinedCost;
+		
 		 heap.push(combinedCost);
     }
 
